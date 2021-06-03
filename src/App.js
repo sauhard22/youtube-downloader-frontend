818e68cd2c48ef58fa8010393810cfe4633a928d
sauhard22 <73341891+sauhard22@users.noreply.github.com>
2021-06-03 17:25:29 +0530

    moved to app.js'

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import './style.css'
 import InputField from './components/InputField/InputField'
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [value, setValue] = useState("")
+  const [data, setData] = useState("")
+
+  function changeHeader(event) {
+    setValue(event.target.value)
+  }
+  function handleClick() {
+    setData(value)
+  }
+
+  useEffect(() => {
+   fetch('https://localhost:4000/')
+   .then(response => response.json)
+   .then(json => console.log(json))
+  })
+
   return (
     <div className="App">
-      <Header/>
-      <Hero/>
+      <Header />
+      <Hero />
       <h1>Enter the link you want to download</h1>
       <div className="int-fld">
-      <InputField/>
+        <InputField value={value} setValue={setValue} data={data} setData={setData} changeHeader={changeHeader} handleClick={handleClick}/>
+        <h6>{data}</h6>
       </div>
     </div>
   );
--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -1,26 +1,12 @@
 import { Button } from 'react-bootstrap'
-import React, {useState} from 'react'
 import './style.css'
 
-const InputField = () => {
-
-    const [value, setValue] = useState("")
-    const [data, setData] = useState("")
-
-    function changeHeader(event) {
-        setValue(event.target.value)
-    }
-    
-    function handleClick(){
-        setData(value)
-    }
+const InputField = ({handleClick, changeHeader}) => {
 
     return (
         <div>
             <input className="int" placeholder="Input Link or ID" onChange={changeHeader}/>
-            <Button className="btn" variant="secondary" onClick={handleClick}>Get Link</Button>
-            <br/>
-            <h6>{data}</h6>
+            <Button className="btn" variant="secondary" onClick={handleClick}>Get Link</Button>        
         </div> 
     )
 }
